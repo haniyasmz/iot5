ee7cbae7d4e01f10641abf7eb5da5aa63e46ff45
haniyasmz <117500943+haniyasmz@users.noreply.github.com>
2023-01-16 12:49:57 +0800

    Update streamlit_sharing.py

--- apabani/streamlit_sharing.py
+++ apabani/streamlit_sharing.py
@@ -41,7 +41,7 @@ engine = create_engine("postgresql+psycopg2://postgres:rocky99@localhost/smart_h
 # cursor = con.cursor()
 # @st.experimental_singleton
 def init_connection():
-    return psycopg2.connect(username=st.secrets.postgres.user, password=st.secrets.postgres.password, host=st.secrets.postgres.host, db=st.secrets.postgres.dbname, port=st.secrets.postgres.port)
+    return psycopg2.connect(user=st.secrets.postgres.user, password=st.secrets.postgres.password, host=st.secrets.postgres.host, db=st.secrets.postgres.dbname, port=st.secrets.postgres.port)
 
 # def init_connection():
 #     return engine
